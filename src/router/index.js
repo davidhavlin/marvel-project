e0d0a8cd250bb2b854bd925f96b8c1affa5e8a0b
davidhavlin <davidhavlin@gmail.com>
2020-12-17 17:14:53 +0100

    router,vuex

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,47 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: '/heroes/:id',
+    name: 'hero-page',
+    props: true,
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import(
+        /* webpackChunkName: "hero-page" */ '../components/Heroes/hero-page.vue'
+      ),
+    beforeEnter: (to, from, next) => {
+      const exists = this.$store.searchResults.find(
+        hero => hero.id === to.params.id
+      )
+      if (exists) {
+        next()
+      } else {
+        next({ name: 'notFound' })
+      }
+    }
   }
-];
+  //   {
+  //     path: "/about",
+  //     name: "About",
+  //     // route level code-splitting
+  //     // this generates a separate chunk (about.[hash].js) for this route
+  //     // which is lazy-loaded when the route is visited.
+  //     component: () =>
+  //       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  //   }
+]
 
 const router = new VueRouter({
   routes
-});
+})
 
-export default router;
+export default router
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,62 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    favorite: [],
+    searchResults: [],
+    lastSearchedWords: []
+  },
+  mutations: {
+    pushWord(state, array) {
+      state.lastSearchedWords = array
+    },
+    pushResults(state, array) {
+      state.searchResults = array
+    },
+    pushFavorite(state, hero) {
+      state.favorite.push(hero)
+    },
+    removeFavorite() {}
+  },
+  actions: {
+    fetchResults(context, payload) {
+      const key = process.env.VUE_APP_MARVELAPI
+      const url = `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${payload}&apikey=${key}`
+      axios
+        .get(url)
+        .then(response => {
+          const results = response.data.data.results
+          console.log(results)
+
+          context.commit('pushResults', results)
+        })
+        .catch(error => {
+          // handle error
+          console.log(error)
+        })
+        .then(() => {
+          // always executed
+        })
+    },
+
+    fiveSearchWords(context, payload) {
+      let oldWords = context.state.lastSearchedWords
+      if (oldWords.indexOf(payload) !== -1) return
+
+      let newArray = []
+      newArray = [...oldWords, payload]
+
+      if (oldWords.length < 5) {
+        context.commit('pushWord', newArray)
+      } else {
+        newArray.shift()
+        context.commit('pushWord', newArray)
+      }
+    }
+  },
   modules: {}
-});
+})

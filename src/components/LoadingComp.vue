831e313662664a1607d1a1cb79bd4d6b89836876
davidhavlin <davidhavlin@gmail.com>
2020-12-18 13:31:13 +0100

    vytvoreny jednoduchy loading komponent

---
+++ src/components/LoadingComp.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="loading">
+    <h3>Loading...</h3>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.loading {
+  h3 {
+    color: #6b88a3;
+    font-weight: 100;
+    font-style: italic;
+  }
+  margin: 4rem;
+  animation: loading 1s infinite ease;
+}
+
+@keyframes loading {
+  50% {
+    transform: scale(1.2);
+  }
+}
+</style>

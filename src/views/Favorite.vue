d09603170e62560dc081b71dadcf002bfc777ab7
davidhavlin <davidhavlin@gmail.com>
2020-12-18 13:32:43 +0100

    spravena favorite stranka

--- src/views/Favorite.vue
+++ src/views/Favorite.vue
@@ -1,12 +1,62 @@
 <template>
-  <article class="favorite"></article>
+  <article class="page-container">
+    <h2 style="textAlign:left">Your Favorite SuperHeroes</h2>
+    <transition-group name="hero" tag="div" class="results-container">
+      <SuperHero v-for="hero in favoriteHeroes" :key="hero.id" :hero="hero" />
+    </transition-group>
+    <div v-if="favoriteHeroes.length < 1" class="no-results">
+      You have no favorite Super Heroes :(
+    </div>
+  </article>
 </template>
 
 <script>
-export default {}
+import SuperHero from '@/components/Home/super-hero.vue'
+
+export default {
+  components: {
+    SuperHero
+  },
+  computed: {
+    favoriteHeroes() {
+      return this.$store.state.favorite
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-.favorite {
+.results-container {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1rem;
+}
+
+.no-results {
+  height: 150px;
+  border-radius: 5px;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  font-style: italic;
+  color: #486786;
+  background: #263645;
+  opacity: 0;
+  animation: fadein 0.5s forwards ease;
+  animation-delay: 0.3s;
+}
+@keyframes fadein {
+  to {
+    opacity: 1;
+  }
+}
+
+.hero-enter-active,
+.hero-leave-active {
+  transition: all 0.3s;
+}
+.hero-enter, .hero-leave-to /* .list-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  transform: scale(0.9);
 }
 </style>

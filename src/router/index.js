ce06d825072b4df40b24780657ff0bbd3aaaab8d
davidhavlin <davidhavlin@gmail.com>
2020-12-18 13:34:52 +0100

    pridana favorite cesta

--- src/router/index.js
+++ src/router/index.js
@@ -13,31 +13,27 @@ const routes = [
   {
     path: '/heroes/:id',
     name: 'hero-page',
-    props: true,
     component: () =>
       import(
         /* webpackChunkName: "hero-page" */ '../components/Heroes/hero-page.vue'
-      ),
-    beforeEnter: (to, from, next) => {
-      const exists = this.$store.searchResults.find(
-        hero => hero.id === to.params.id
       )
-      if (exists) {
-        next()
-      } else {
-        next({ name: 'notFound' })
-      }
-    }
+    // beforeEnter: (to, from, next) => {
+    //   const exists = this.$store.searchResults.find(
+    //     hero => hero.id === to.params.id
+    //   )
+    //   if (exists) {
+    //     next()
+    //   } else {
+    //     next({ name: 'notFound' })
+    //   }
+    // }
+  },
+  {
+    path: '/favorite',
+    name: 'Favorite',
+    component: () =>
+      import(/* webpackChunkName: "favorite" */ '../views/Favorite.vue')
   }
-  //   {
-  //     path: "/about",
-  //     name: "About",
-  //     // route level code-splitting
-  //     // this generates a separate chunk (about.[hash].js) for this route
-  //     // which is lazy-loaded when the route is visited.
-  //     component: () =>
-  //       import(/* webpackChunkName: "about" */ "../views/About.vue")
-  //   }
 ]
 
 const router = new VueRouter({

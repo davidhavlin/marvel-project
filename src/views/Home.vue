03e5f2d30dad659f9e5f0168e4af400aa7cb9ae6
davidhavlin <davidhavlin@gmail.com>
2020-12-19 16:41:40 +0100

    pridany welcome container

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,8 +6,15 @@
     </h3>
     <div v-show="!loading" class="results-container">
       <SuperHero v-for="hero in searchResults" :key="hero.id" :hero="hero" />
-      <div v-if="noResults()" class="no-results">
+      <div v-if="noResults() && !firstVisit" class="no-results">
         No results for "{{ lastWord }}"
+      </div>
+      <div v-if="firstVisit" class="welcome-container">
+        <div class="welcome-content">
+          Welcome to <NavLogo class="logo" /> <span>Super Heroes Website</span>
+          <br />
+          search for your favorite heroes!
+        </div>
       </div>
     </div>
   </article>
@@ -16,16 +23,22 @@
 <script>
 import SuperHero from '@/components/Home/super-hero.vue'
 import LoadingComp from '@/components/LoadingComp.vue'
+import NavLogo from '@/components/Navigation/nav-logo.vue'
 
 export default {
   name: 'Home',
   components: {
     SuperHero,
-    LoadingComp
+    LoadingComp,
+    NavLogo
   },
   computed: {
     searchResults() {
       return this.$store.state.searchResults
+    },
+
+    firstVisit() {
+      return this.$store.state.firstVisit
     },
 
     lastWord() {
@@ -52,7 +65,7 @@ export default {
 }
 .results-container {
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
   gap: 1rem;
 }
 
@@ -66,5 +79,28 @@ export default {
   font-style: italic;
   color: #486786;
   background: #263645;
+}
+
+.welcome-container {
+  grid-column: 1 / -1;
+  font-size: 2em;
+  line-height: 4rem;
+  color: #7192b1;
+  height: 400px;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+
+  span {
+    color: #fff;
+    font-weight: bold;
+  }
+
+  .logo {
+    transform: none;
+    pointer-events: none;
+    display: inline-block;
+    transform: translateY(14px);
+  }
 }
 </style>

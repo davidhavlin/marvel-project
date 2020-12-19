5a1b83c9102546b32eef12424709023408e390be
davidhavlin <davidhavlin@gmail.com>
2020-12-19 16:41:19 +0100

    pridana notFound stranka

---
+++ src/views/notFound.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="page-container">
+    <div class="results-container">
+      <div class="no-results">
+        hero does not exist :[
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.results-container {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 1rem;
+}
+
+.no-results {
+  grid-column: 1 / -1;
+  height: 150px;
+  border-radius: 5px;
+  display: grid;
+  justify-content: center;
+  align-content: center;
+  font-style: italic;
+  color: #486786;
+  background: #263645;
+}
+</style>
